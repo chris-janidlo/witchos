ed2f7855f3f44fb5f1da5929a05d73ae812af27c
crassSandwich <christopherjanidlo@gmail.com>
2020-03-29 13:44:28 -0400

    Window clamping

---
+++ Assets/Scripts/Window System/ClampToParent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class ClampToParent : MonoBehaviour
+{
+    void Update ()
+    {
+        var rectTransform = GetComponent<RectTransform>();
+        var parentRect = rectTransform.parent as RectTransform;
+
+        Vector3 minPosition = parentRect.rect.min - rectTransform.rect.min;
+        Vector3 maxPosition = parentRect.rect.max - rectTransform.rect.max;
+
+        rectTransform.localPosition = new Vector3
+        (
+            Mathf.Clamp(rectTransform.localPosition.x, minPosition.x, maxPosition.x),
+            Mathf.Clamp(rectTransform.localPosition.y, minPosition.y, maxPosition.y),
+            rectTransform.localPosition.z
+        );
+    }
+}
