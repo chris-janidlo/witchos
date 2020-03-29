d1c8fd3240ccf7279a5e731e695934d3ab889b64
crassSandwich <christopherjanidlo@gmail.com>
2020-03-29 14:33:51 -0400

    Icons for applications

---
+++ Assets/Scripts/Desktop/AppIcon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppIcon : MonoBehaviour
+{
+    public Window WindowPrefab;
+
+    public void LaunchApplication ()
+    {
+        WindowFactory.Instance.CreateWindowWithTaskbarButton(WindowPrefab);
+    }
+}
---
+++ Assets/Scripts/Desktop/DesktopIcon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using TMPro;
+
+public class DesktopIcon : MonoBehaviour
+{
+    [Header("Data")]
+    public Sprite Icon;
+    public string Label;
+    public UnityEvent OnClick;
+
+    [Header("References")]
+    public Image IconImage;
+    public TextMeshProUGUI LabelText;
+    public Button Button;
+
+    void Start ()
+    {
+        Button.onClick.AddListener(OnClick.Invoke);
+    }
+
+    void Update ()
+    {
+        IconImage.sprite = Icon;
+        LabelText.text = Label;
+    }
+}
