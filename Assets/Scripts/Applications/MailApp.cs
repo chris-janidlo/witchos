d864056dfa07b5c6f5ae432656fb02d0c8baeae1
crassSandwich <christopherjanidlo@gmail.com>
2020-03-30 19:25:20 -0400

    Minor cleanup/rearranging

--- Assets/Scripts/Applications/MailApp.cs
+++ Assets/Scripts/Applications/MailApp.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Mail : MonoBehaviour
+public class MailApp : MonoBehaviour
 {
     public Window Window;
     public Sprite ReadIcon, UnreadIcon;
