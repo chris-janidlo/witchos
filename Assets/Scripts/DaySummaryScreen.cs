41f0b3f6705aa1b35ecad7a4530f1b87f2fe0f62
crassSandwich <christopherjanidlo@gmail.com>
2020-03-30 20:41:04 -0400

    Day cycle

---
+++ Assets/Scripts/DaySummaryScreen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using crass;
+
+public class DaySummaryScreen : Singleton<DaySummaryScreen>
+{
+    public RectTransform Container;
+    public TextMeshProUGUI TasksCompleted, Revenue, Expenses, Total;
+    public Button DoneButton;
+
+    void Awake ()
+    {
+        SingletonOverwriteInstance(this);
+
+        DoneButton.onClick.AddListener(closeSummary);
+        Container.gameObject.SetActive(false);
+    }
+
+    public void ShowSummary ()
+    {
+        // TODO:
+        // TasksCompleted.text = 
+        // Revenue.text = 
+        // Total.text = 
+
+        Container.gameObject.SetActive(true);
+    }
+
+    void closeSummary ()
+    {
+        Container.gameObject.SetActive(false);
+        TimeState.Instance.StartNewDay();
+    }
+}
--- Assets/Scripts/Game State/TimeState.cs
+++ Assets/Scripts/Game State/TimeState.cs
@@ -8,9 +8,15 @@ using crass;
 public class TimeState : Singleton<TimeState>
 {
     // Friday the 13th in October, also happens to be a full moon, and on the brink of the millennium, pretty neat
-    public static DateTime InitialDateTime = new DateTime(2000, 10, 13, 18, 0, 0);
+    public static DateTime INITIAL_DATE = new DateTime(2000, 10, 13);
 
-    public DateTime DateTime { get; private set; }
+    public static int
+        DAY_START_HOUR = 18,
+        DAY_END_HOUR = 23;
+
+    public DateTime DateTime { get; private set; } = INITIAL_DATE.AddHours(DAY_START_HOUR);
+
+    public event Action DayStarted;
 
     public float InGameSecondsPerRealtimeSeconds;
     [TextArea]
@@ -19,13 +25,26 @@ public class TimeState : Singleton<TimeState>
     void Awake ()
     {
         SingletonSetPersistantInstance(this);
-
-        DateTime = InitialDateTime;
     }
 
     void Update ()
     {
         DateTime = DateTime.AddSeconds(InGameSecondsPerRealtimeSeconds * Time.deltaTime);
+
+        // TODO: add warning before abruptly ending
+        if (DateTime.Hour >= DAY_END_HOUR)
+        {
+            Time.timeScale = 0;
+            DaySummaryScreen.Instance.ShowSummary();
+        }
+    }
+
+    public void StartNewDay ()
+    {
+        DateTime = DateTime.AddDays(1);
+        DateTime = DateTime.AddHours(DAY_START_HOUR - DAY_END_HOUR);
+        Time.timeScale = 1;
+        DayStarted?.Invoke();
     }
 
     public string GetTimeString ()
@@ -36,14 +55,14 @@ public class TimeState : Singleton<TimeState>
     // yeah yeah smelly I know
     public MoonPhase GetTodaysMoonPhase ()
     {
-        int daysElapsed = (DateTime.Date - InitialDateTime.Date).Days;
+        int daysElapsed = (DateTime.Date - INITIAL_DATE.Date).Days;
         return (MoonPhase) (daysElapsed % EnumUtil.NameCount<MoonPhase>());
     }
 
     // yeah yeah smelly I know
     public MoonPhase GetTomorrowsMoonPhase ()
     {
-        int daysElapsed = (DateTime.Date - InitialDateTime.Date).Days + 1;
+        int daysElapsed = (DateTime.Date - INITIAL_DATE.Date).Days + 1;
         return (MoonPhase) (daysElapsed % EnumUtil.NameCount<MoonPhase>());
     }
 }
