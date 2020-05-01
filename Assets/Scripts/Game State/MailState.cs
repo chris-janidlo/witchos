17e77ffc17fd2a84551f100a433cf18fdd05a58c
crassSandwich <christopherjanidlo@gmail.com>
2020-04-30 21:22:40 -0400

    Simplify unnecessary AnimationCurve usage, ramp up difficulty

--- Assets/Scripts/Game State/MailState.cs
+++ Assets/Scripts/Game State/MailState.cs
@@ -11,7 +11,7 @@ public class MailState : Singleton<MailState>
     public class MailBag : BagRandomizer<Invoice> {}
 
     public MailBag PossibleEMails;
-    public AnimationCurve EMailsToSpawnByDifficulty;
+    public List<int> EMailsToSpawnByDifficulty;
     public int CurrentDifficultyLevel; // the highest level of difficulty that has been completed in one day
 
     public int TasksCompleted, TotalTasks;
@@ -20,7 +20,7 @@ public class MailState : Singleton<MailState>
 
     public int UnreadMessageCount => CurrentMessages.Where(m => !m.Read).Count();
 
-    int difficulty => (int) EMailsToSpawnByDifficulty.Evaluate(CurrentDifficultyLevel);
+    int spawnCount => EMailsToSpawnByDifficulty[CurrentDifficultyLevel];
 
     void Awake ()
     {
@@ -34,12 +34,12 @@ public class MailState : Singleton<MailState>
 
     public void StartDay ()
     {
-        if (TasksCompleted == difficulty)
+        if (TasksCompleted == spawnCount)
         {
             CurrentDifficultyLevel++;
         }
 
-        while (CurrentMessages.Count < difficulty)
+        while (CurrentMessages.Count < spawnCount)
         {
             // kind of a kludge right now. all of the emails we currently have are repeatable filler emails that the player can and will see more than once. we want to preserve the unread state of the email across in-game days and across mail app sessions, and the only real way to do that is by storing the read state in the email object itself. that said, we don't want to store it on the ScriptableObject instance, since that preserves the read state across repeats of the same email, which is totally undesired behavior. so we create a copy of the email to add and add the copy. this is definitely not how we want to handle conversations or other one-off emails in the future
             var newMessageClone = Instantiate(PossibleEMails.GetNext());
@@ -47,7 +47,7 @@ public class MailState : Singleton<MailState>
         }
 
         TasksCompleted = 0;
-        TotalTasks = difficulty;
+        TotalTasks = spawnCount;
     }
 
     void onSpellCast (Spell spell)
