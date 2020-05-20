ca4c7903248e7f53050aa897658fbf679c74cb94
crassSandwich <christopherjanidlo@gmail.com>
2020-05-19 23:34:16 -0400

    Basic invoice inbox level stuff

---
+++ Assets/Scripts/Applications/OrderPreview.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class OrderPreview : MonoBehaviour
+{
+    public Sprite FlashOnSprite, FlashOffSprite, NormalSprite;
+    public float FlashSecondsPerSprite;
+
+    public Window DetailsPrefab;
+
+    public TextMeshProUGUI Buyer, ItemCount, DueDate, TotalValue;
+
+    public Button Button;
+    public Image ButtonImage;
+
+    Invoice invoice;
+
+    float flashTimer;
+    bool flashState;
+
+    void Start ()
+    {
+        Button.onClick.AddListener(openInvoice);
+    }
+
+    void Update ()
+    {
+        bool read = OrderState.Instance.InvoiceInbox.GetReadState(invoice);
+
+        if (read)
+        {
+            ButtonImage.sprite = NormalSprite;
+        }
+        else
+        {
+            flashTimer -= Time.deltaTime;
+
+            if (flashTimer <= 0)
+            {
+                flashState = !flashState;
+                flashTimer = FlashSecondsPerSprite;
+            }
+
+            ButtonImage.sprite = flashState ? FlashOnSprite : FlashOffSprite;
+        }
+    }
+
+    public void SetInvoice (Invoice invoice)
+    {
+        this.invoice = invoice;
+
+        Buyer.text = invoice.BuyerName;
+        ItemCount.text = "1 item";
+        DueDate.text = "No Due Date";
+        TotalValue.text = "Gratis";
+    }
+
+    void openInvoice ()
+    {
+        WindowFactory.Instance.OpenWindow(DetailsPrefab, invoice, "invoice " + invoice.GetHashCode().ToString(), WindowFactory.Options.Singleton | WindowFactory.Options.TaskBarButton);
+        OrderState.Instance.InvoiceInbox.SetReadState(invoice, true);
+    }
+}
