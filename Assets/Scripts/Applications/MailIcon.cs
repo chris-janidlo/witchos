1a3865904842d51c8a67965ba4114570544a3bf3
crassSandwich <christopherjanidlo@gmail.com>
2020-05-28 23:09:34 -0400

    Implement Mail Icon flashing

---
+++ Assets/Scripts/Applications/MailIcon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MailIcon : MonoBehaviour
+{
+    public Sprite OnSprite, OffSprite;
+    public float FlashTime;
+    public DesktopIcon Icon;
+
+    IEnumerator Start ()
+    {
+        while (true)
+        {
+            if (MailState.Instance.UnreadMessageCount == 0)
+            {
+                Icon.Icon = OffSprite;
+                yield return null;
+            }
+            else
+            {
+                Icon.Icon = OnSprite;
+                yield return new WaitForSeconds(FlashTime);
+                Icon.Icon = OffSprite;
+                yield return new WaitForSeconds(FlashTime);
+            }
+        }
+    }
+}
