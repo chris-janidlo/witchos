fec9ea4ba3c7b5b1c01a569365a8faac5f10cfe6
crassSandwich <christopherjanidlo@gmail.com>
2020-05-30 00:49:40 -0400

    Money (#4)

---
+++ Assets/Scripts/Applications/BankApp.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BankApp : MonoBehaviour
+{
+    public BankAppTransactionLogEntry LogEntryPrefab;
+    public VerticalLayoutGroup EntryLog;
+    public TextMeshProUGUI BalanceDisplay;
+
+    int entriesInLog;
+
+    void Update ()
+    {
+        BalanceDisplay.text = BankState.Instance.CurrentBalance.ToString();
+
+        if (entriesInLog != BankState.Instance.Transactions.Count)
+            populateLog();
+    }
+
+    void populateLog ()
+    {
+        entriesInLog = BankState.Instance.Transactions.Count;
+
+        foreach (Transform child in EntryLog.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (BankTransaction transaction in BankState.Instance.Transactions.Reverse())
+        {
+            Instantiate(LogEntryPrefab, EntryLog.transform).SetTransaction(transaction);
+        }
+    }
+}
---
+++ Assets/Scripts/Game State/BankState.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using crass;
+
+public class BankState : Singleton<BankState>
+{
+    public int CurrentBalance => transactions.Sum(t => t.DeltaCurrency);
+
+    public string InsufficientFundsAlertMessage;
+
+    [SerializeField]
+    List<BankTransaction> transactions = new List<BankTransaction>();
+    public IReadOnlyList<BankTransaction> Transactions => transactions.AsReadOnly();
+
+    void Awake ()
+    {
+        SingletonOverwriteInstance(this);
+    }
+
+    public bool AddTransaction (int deltaCurrency, string description, bool autoAlert = true)
+    {
+        if (!HaveEnoughMoney(deltaCurrency))
+        {
+            if (autoAlert) Alert.Instance.ShowMessageImmediately(InsufficientFundsAlertMessage);
+            return false;
+        }
+
+        BankTransaction transaction = new BankTransaction
+        {
+            InitialCurrency = CurrentBalance,
+            DeltaCurrency = deltaCurrency,
+            Description = description,
+            Date = TimeState.Instance.DateTime.Date
+        };
+
+        transactions.Add(transaction);
+
+        return true;
+    }
+
+    public bool HaveEnoughMoney (int deltaCurrency)
+    {
+        if (deltaCurrency >= 0) return true;
+        else return CurrentBalance + deltaCurrency >= 0;
+    }
+}
