79684a27af6931a5a2e13f20a481c9f978e9706b
crassSandwich <christopherjanidlo@gmail.com>
2020-05-30 00:49:07 -0400

    Bank application

---
+++ Assets/Scripts/Applications/BankAppTransactionLogEntry.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Globalization;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BankAppTransactionLogEntry : MonoBehaviour
+{
+    public TextMeshProUGUI DateText, DescriptionText, AmountText, BalanceText;
+
+    public void SetTransaction (BankTransaction transaction)
+    {
+        DateText.text = transaction.Date.ToString("d", DateTimeFormatInfo.InvariantInfo);
+        DescriptionText.text = transaction.Description;
+        AmountText.text = transaction.DeltaCurrency.ToString("+#;-#;0"); // from https://stackoverflow.com/a/348242/5931898
+        BalanceText.text = (transaction.InitialCurrency + transaction.DeltaCurrency).ToString();
+    }
+}
