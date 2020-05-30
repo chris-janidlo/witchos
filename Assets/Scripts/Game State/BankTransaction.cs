cd12369d43824cecc42230679e75df5b346bb476
crassSandwich <christopherjanidlo@gmail.com>
2020-05-29 22:25:56 -0400

    Bank state

---
+++ Assets/Scripts/Game State/BankTransaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct BankTransaction
+{
+    public int InitialCurrency, DeltaCurrency;
+    public string Description;
+
+    [SerializeField]
+    string dateString;
+    public DateTime Date
+    {
+        get => DateTime.Parse(dateString);
+        set => dateString = value.ToString();
+    }
+}
