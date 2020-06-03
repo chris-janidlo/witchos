a83eb106aedf56565c4573f76dac8c9331442b37
crassSandwich <christopherjanidlo@gmail.com>
2020-06-03 00:18:59 -0400

    Initial skeleton

---
+++ Assets/Scripts/Game State/SaveData.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SaveData
+{
+    public const string FILE_EXTENSION = ".witchos";
+
+    public abstract void InitializeData ();
+    public abstract void WriteDataToFile ();
+    public abstract void DeleteSaveFile ();
+}
+
+public class SaveData<T> : SaveData
+    // where T : BinarySerializable
+{
+    T value;
+    public T Value
+    {
+        get
+        {
+            if (!DataInitialized) InitializeData();
+            return value;
+        }
+
+        set
+        {
+            this.value = value;
+            DataChangedSinceLastWrite = true;
+        }
+    }
+
+    // the part not including the parent directory or the file extension
+    public string FileName { get; private set; }
+    public string FilePath => Path.Combine(Application.persistentDataPath, FileName + FILE_EXTENSION);
+    
+    public bool DataChangedSinceLastWrite { get; private set; }
+    public bool DataInitialized { get; private set; }
+
+    // default value before any save interactions have ocurred
+    T gameDefaultValue;
+
+    public SaveData (string fileName, T gameDefaultValue)
+    {
+        FileName = fileName;
+        this.gameDefaultValue = gameDefaultValue;
+    }
+
+    public override void InitializeData ()
+    {
+        // if file doesn't exist: set value to gameDefaultValue
+        // else: load the data from FilePath
+
+        DataInitialized = true;
+    }
+
+    public override void WriteDataToFile ()
+    {
+        if (!DataChangedSinceLastWrite) return;
+
+        // save the data to FilePath
+    }
+
+    public override void DeleteSaveFile ()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Game State/SaveManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SaveManager
+{
+    public static SaveData<LooseSaveValues> LooseSaveData { get; private set; }
+    public static SaveData<List<BankTransaction>> BankTransactionData { get; private set; }
+    public static SaveData<List<Invoice>> EMailData { get; private set; }
+
+    static List<SaveData> allDataObjects;
+
+    static SaveManager ()
+    {
+        LooseSaveData = new SaveData<LooseSaveValues>
+        (
+            "looseData",
+            new LooseSaveValues { Date = new DateTime(2000, 10, 13) }
+        );
+
+        BankTransactionData = new SaveData<List<BankTransaction>>
+        (
+            "bankTransactions",
+            new List<BankTransaction>
+            {
+                new BankTransaction
+                {
+                    InitialCurrency = 0,
+                    DeltaCurrency = 100,
+                    Description = "Initial Deposit",
+                    Date = new DateTime(2000, 10, 13)
+                }
+            }
+        );
+
+        EMailData = new SaveData<List<Invoice>>
+        (
+            "emailData",
+            new List<Invoice>()
+        );
+
+        allDataObjects = new List<SaveData>
+        {
+            LooseSaveData, BankTransactionData, EMailData
+        };
+    }
+
+    public static void SaveAllData ()
+    {
+        allDataObjects.ForEach(s => s.WriteDataToFile());
+    }
+
+    public static void DeleteAllSaveData ()
+    {
+        allDataObjects.ForEach(s => s.DeleteSaveFile());
+    }
+}
+
+public struct LooseSaveValues
+{
+    public DateTime Date;
+    // list of icon positions
+}
