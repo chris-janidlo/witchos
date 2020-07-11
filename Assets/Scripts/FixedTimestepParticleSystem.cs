43de2f3efd13ae2760439c9e8b4348b6348585d6
Chris Janidlo <christopherjanidlo@gmail.com>
2020-07-10 23:02:05 -0400

    Make it less smooth
    So it doesn't feel out of place in a pixel animation

---
+++ Assets/Scripts/FixedTimestepParticleSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WitchOS
+{
+    // for pixel animation feel
+    public class FixedTimestepParticleSystem : MonoBehaviour
+    {
+        public float Timestep;
+        public ParticleSystem ParticleSystem;
+
+        float timestepTimer;
+
+        void Update ()
+        {
+            if (!ParticleSystem.emission.enabled && ParticleSystem.particleCount == 0) return;
+
+            timestepTimer += Time.deltaTime;
+
+            if (timestepTimer >= Timestep)
+            {
+                ParticleSystem.Simulate(timestepTimer, true, false);
+                timestepTimer = 0;
+            }
+        }
+    }
+}
