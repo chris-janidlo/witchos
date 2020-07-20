47f064282472ee6188cf5430fa135b19c47f32e8
Chris Janidlo <christopherjanidlo@gmail.com>
2020-07-19 23:31:43 -0400

    Scaffolding and first draft of homepage for Wiki stuff

---
+++ Assets/Scripts/Applications/WikiApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+using crass;
+
+namespace WitchOS
+{
+    public class WikiApp : MonoBehaviour, IPointerUpHandler
+    {
+        public WikiPage HomePage;
+
+        public Window Window;
+        public TextMeshProUGUI PageText;
+
+        void Start ()
+        {
+            drawPage(HomePage);
+        }
+
+        public void OnPointerUp (PointerEventData eventData)
+        {
+            if (!Window.Focused) return;
+
+            int linkIndex = TMP_TextUtilities.FindIntersectingLink(PageText, Input.mousePosition, CameraCache.Main);
+
+            if (linkIndex < 0) return;
+
+            string linkID = PageText.textInfo.linkInfo[linkIndex].GetLinkID();
+            drawPage(WikiPage.LookUpTable[linkID]);
+        }
+
+        void drawPage (WikiPage page)
+        {
+            // TODO build page from prefab building blocks
+        }
+    }
+}
---
+++ Assets/Scripts/Game State/WikiPage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WitchOS
+{
+    [CreateAssetMenu(fileName = "NewWikiPage.asset", menuName = "WitchOS/Wiki Page")]
+    public class WikiPage : ScriptableObject
+    {
+        public static Dictionary<string, WikiPage> LookUpTable = new Dictionary<string, WikiPage>();
+
+        [Serializable]
+        public class ContentSection
+        {
+            public string Title;
+            [TextArea(5, 100)]
+            public string Content;
+        }
+
+        [TextArea(5, 100)]
+        public string LeadSection;
+
+        public List<ContentSection> BodySections;
+
+        public string Title => name;
+
+        void OnEnable ()
+        {
+            if (LookUpTable.ContainsKey(Title))
+            {
+                throw new InvalidOperationException($"attempted to add two pages with the same title ({Title})");
+            }
+
+            LookUpTable[Title] = this;
+        }
+    }
+}
