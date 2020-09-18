9ef4c475c5985e6e247bc8a1357a3a09b7be3f16
crass_sandwich <christopherjanidlo@gmail.com>
2020-09-17 20:57:48 -0400

    Window canvas implementation, offsets for child objects

---
+++ Assets/Scripts/Desktop/SortingPlaneOffsetter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WitchOS
+{
+    public class SortingPlaneOffsetter : MonoBehaviour
+    {
+        public int Offset;
+        public Renderer Renderer;
+
+        public void UpdateSortingOrder (int sortingPlaneIndex)
+        {
+            Renderer.sortingOrder = sortingPlaneIndex + Offset;
+        }
+    }
+}
