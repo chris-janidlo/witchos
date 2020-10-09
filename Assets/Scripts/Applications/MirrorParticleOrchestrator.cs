f8c5e8a7756f691e43bd4daac1d5424c38d17c4b
crass_sandwich <christopherjanidlo@gmail.com>
2020-10-08 22:12:08 -0400

    Also make particles look nice when opening and closing the app

---
+++ Assets/Scripts/Applications/MirrorParticleOrchestrator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WitchOS
+{
+    public class MirrorParticleOrchestrator : MonoBehaviour
+    {
+        public ParticleSystem MainSystem, TrailSystem, BurstSystem;
+
+        public void StartParticlesWithBurst ()
+        {
+            setMainPrewarm(false);
+            MainSystem.Play();
+            BurstSystem.Play();
+            TrailSystem.Play();
+        }
+
+        public void StartParticlesWithoutBurst ()
+        {
+            setMainPrewarm(true);
+            MainSystem.Play();
+            TrailSystem.Play();
+        }
+
+        public void StopParticles ()
+        {
+            MainSystem.Stop();
+            TrailSystem.Stop();
+        }
+
+        void setMainPrewarm (bool value)
+        {
+            var mainMain = MainSystem.main;
+            mainMain.prewarm = value;
+        }
+    }
+}
