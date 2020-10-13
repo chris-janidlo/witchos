b71d062017e569c0c2e670d285705915115d5ea9
crass_sandwich <christopherjanidlo@gmail.com>
2020-10-12 20:24:10 -0400

    Create tool to reserialize assets

---
+++ Assets/Scripts/Editor/ForceReserializeTool.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ForceReserializeTool : MonoBehaviour
+{
+    [MenuItem("Tools/Force reserialize all assets")]
+    public static void Reserialize ()
+    {
+        AssetDatabase.ForceReserializeAssets();
+    }
+}
