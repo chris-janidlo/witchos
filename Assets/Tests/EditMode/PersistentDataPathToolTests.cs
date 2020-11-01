31842c1ee99219538ee07d1c9dd8a2c164f2112c
crass_sandwich <christopherjanidlo@gmail.com>
2020-11-01 15:03:37 -0500

    Write a simple edit mode test

---
+++ Assets/Tests/EditMode/PersistentDataPathToolTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using WitchOS.Editor;
+
+namespace Tests
+{
+    public class PersistentDataPathToolTests
+    {
+        [Test]
+        public void CorrectPathIsCopiedToBuffer ()
+        {
+            EditorGUIUtility.systemCopyBuffer = "";
+            PersistentDataPathTool.CopyPersistentDataPath();
+            Assert.AreEqual(Application.persistentDataPath, EditorGUIUtility.systemCopyBuffer);
+        }
+    }
+}
