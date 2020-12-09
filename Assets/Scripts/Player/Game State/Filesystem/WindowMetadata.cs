93c7a9ede852a4495231e2f55ba7a51e86703bef
crass_sandwich <christopherjanidlo@gmail.com>
2020-12-08 22:53:19 -0500

    Add class for holding metadata about how to open a window for a given file

---
+++ Assets/Scripts/Player/Game State/Filesystem/WindowMetadata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WitchOS
+{
+    public class WindowMetadata
+    {
+        public const string FILENAME_PLACEHOLDER = "##file##";
+
+        public enum NewWindowBehavior
+        {
+            FocusOldWindow, OpenNewWindowPerFile, AlwaysOpenNewWindow
+        }
+
+        [Header("Behavior")]
+        public Window WindowPrefab;
+        public NewWindowBehavior NewWindowMode;
+        public bool AddButtonToTaskbar = true;
+
+        [Header("Presentation")]
+        public Sprite Icon;
+        public string NameTemplate;
+
+        public string GetWindowName (string filename)
+        {
+            return NameTemplate.Replace(FILENAME_PLACEHOLDER, filename);
+        }
+    }
+}
