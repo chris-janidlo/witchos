f04d91fbebc44efdb431998ba310aada5fd883b8
crass_sandwich <christopherjanidlo@gmail.com>
2020-12-08 22:54:27 -0500

    Add config for holding window metadata for non-exe files

---
+++ Assets/Scripts/Player/Game State/Filesystem/FileAssociationConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WitchOS
+{
+    public class FileAssociationConfig : ScriptableObject
+    {
+        // TODO: might need some kind of generic fallback. alternatively, could have one association with type "object"
+
+        public class FileAssociationData
+        {
+            public Type DataType;
+            public WindowMetadata IconMetadata;
+        }
+
+        public List<FileAssociationData> Config;
+
+        public WindowMetadata GetFileAssociations (Type fileDataType)
+        {
+            return Config.FirstOrDefault(d => d.DataType == fileDataType).IconMetadata;
+        }
+
+        public WindowMetadata GetFileAssociations<T> ()
+        {
+            return GetFileAssociations(typeof(T));
+        }
+    }
+}
