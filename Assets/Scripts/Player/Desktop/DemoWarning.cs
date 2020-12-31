032fefb2bb899f77ba1d1588dff7dc8e375666bf
crass_sandwich <christopherjanidlo@gmail.com>
2020-12-30 22:10:45 -0500

    Two flavors of save warning
    Initial warning, and watermark-esk reminder

---
+++ Assets/Scripts/Player/Desktop/DemoWarning.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WitchOS
+{
+    public class DemoWarning : MonoBehaviour
+    {
+        [TextArea]
+        public string WarningText;
+
+        public float Delay;
+
+        bool warned;
+
+        public void OnNewDay ()
+        {
+#if UNITY_WEBGL
+            if (warned) return;
+            warned = true;
+            StartCoroutine(warnRoutine());
+        }
+
+        IEnumerator warnRoutine ()
+        { 
+            yield return new WaitForSeconds(Delay);
+            Alert.Instance.ShowMessageImmediately(WarningText);
+#endif // UNITY_WEBGL
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Desktop/DemoWatermark.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using crass;
+
+namespace WitchOS
+{
+    public class DemoWatermark : MonoBehaviour
+    {
+        public string WatermarkText;
+        public TransitionableFloat XOffset;
+        public Vector2 Origin;
+        public float OffscreenDistance, OffscreenTime, OnscreenTime;
+
+        public TextMeshProUGUI WatermarkTextObject;
+        public ParticleSystem WatermarkTrailParticles;
+
+#if UNITY_WEBGL
+        IEnumerator Start ()
+        {
+            WatermarkTextObject.text = WatermarkText;
+            WatermarkTextObject.transform.localPosition = Origin;
+
+            XOffset.AttachMonoBehaviour(this);
+
+            while (true)
+            {
+                yield return new WaitForSeconds(OnscreenTime);
+                yield return moveRoutine(Origin.x + OffscreenDistance);
+                
+                yield return new WaitForSeconds(OffscreenTime);
+
+                WatermarkTrailParticles.Stop();
+                XOffset.Value = Origin.x - OffscreenDistance;
+                yield return null;
+
+                WatermarkTrailParticles.Play();
+                yield return moveRoutine(Origin.x);
+            }
+        }
+
+        IEnumerator moveRoutine (float targetX)
+        {
+            XOffset.StartTransitionTo(targetX);
+            while (XOffset.Transitioning)
+            {
+                WatermarkTextObject.transform.localPosition = new Vector2(Mathf.Round(XOffset.Value), Origin.y);
+                yield return null;
+            }
+        }
+#endif // UNITY_WEBGL
+    }
+}
