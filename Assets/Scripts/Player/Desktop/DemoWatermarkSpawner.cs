d5e1ea16f7b852014ebd3df8cb9e80074cf951f8
crassSandwich <christopherjanidlo@gmail.com>
2020-12-30 23:00:08 -0500

    Feature/demo save warning (#38)

---
+++ Assets/Scripts/Player/Desktop/DemoWatermarkSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using crass;
+
+namespace WitchOS
+{
+    public class DemoWatermarkSpawner : MonoBehaviour
+    {
+        public DemoWatermark WatermarkPrefab;
+
+#if UNITY_WEBGL
+        void Start ()
+        {
+            Instantiate(WatermarkPrefab, transform);
+        }
+#endif // UNITY_WEBGL
+    }
+}
