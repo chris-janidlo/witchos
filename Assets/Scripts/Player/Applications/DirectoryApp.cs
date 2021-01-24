8a12e8fc948b9229dc16a81429c46c4fc67726cb
crass_sandwich <christopherjanidlo@gmail.com>
2021-01-24 13:43:06 -0500

    Add scroll rect and draw icons in directory

---
+++ Assets/Scripts/Player/Applications/DirectoryApp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WitchOS
+{
+    public class DirectoryApp : MonoBehaviour
+    {
+        public Window Window;
+        public DirectoryDrawer DirectoryDrawer;
+
+        void Start ()
+        {
+            DirectoryDrawer.Draw(Window.File as Directory);
+        }
+    }
+}
