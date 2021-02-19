e504bf19a699480badb90a2d5d92b34b3bbd80c4
crass_sandwich <christopherjanidlo@gmail.com>
2021-02-19 15:37:19 -0500

    Initialize SpellEther in a dedicated thing

---
+++ Assets/Scripts/Player/Game State/SpellEtherClearer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityAtoms.WitchOS;
+
+namespace WitchOS
+{
+    public class SpellEtherClearer : MonoBehaviour
+    {
+        public SpellDeliverableValueList SpellEther;
+
+        void Awake ()
+        {
+            SpellEther.Clear();
+        }
+
+        public void OnDayEnded ()
+        {
+            SpellEther.Clear();
+        }
+    }
+}
