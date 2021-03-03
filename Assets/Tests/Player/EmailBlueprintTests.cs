e37314140b0b339e8647093c974e04b6f1bcb55a
crass_sandwich <christopherjanidlo@gmail.com>
2021-03-03 15:40:35 -0500

    Single order tests

--- Assets/Tests/Player/EmailBlueprintTests.cs
+++ Assets/Tests/Player/EmailBlueprintTests.cs
@@ -8,10 +8,13 @@ namespace WitchOS.Tests
 {
     public class EmailBlueprintTests
     {
+        TimeState timestate;
+
         [SetUp]
         public void SetUp ()
         {
-            Resources.Load<TimeState>("ScriptableObjects/Test Time State").Initialize();
+            timestate = Resources.Load<TimeState>("ScriptableObjects/Test Time State");
+            timestate.Initialize();
         }
 
         [Test]
@@ -33,6 +36,29 @@ namespace WitchOS.Tests
 
             Assert.That(email.SenderAddress, Is.Not.Null);
             Assert.That(email.SenderAddress, Is.Not.Empty);
+
+            var secondEmail = blueprint.GenerateEmail();
+            Assert.That(secondEmail, Is.EqualTo(email));
+        }
+
+        [Test]
+        public void BasicOrderBlueprint_GeneratesSingleOrder ()
+        {
+            var blueprint = loadEmailBlueprint("Single Order");
+            Assert.That(blueprint.PossibleEmails.Items.Count, Is.EqualTo(1));
+            Assert.That(blueprint.PossibleInvoices.Items.Count, Is.EqualTo(1));
+
+            var email = blueprint.GenerateEmail();
+
+            Assert.That(email, Is.InstanceOf<Email>());
+            Assert.That(email, Is.InstanceOf<Order>());
+
+            var order = email as Order;
+
+            Assert.That(order.Invoice, Is.Not.Null);
+            Assert.That(blueprint.PossibleInvoices.Items[0].FullDaysToComplete, Is.EqualTo(0));
+            Assert.That(order.DueDate, Is.EqualTo(timestate.DateTime));
+            Assert.That(order.State, Is.EqualTo(OrderState.InProgress));
 
             var secondEmail = blueprint.GenerateEmail();
             Assert.That(secondEmail, Is.EqualTo(email));
