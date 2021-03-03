94c5cb63573783a535819c62bfb8aa4ec7b8ac35
crass_sandwich <christopherjanidlo@gmail.com>
2021-03-03 15:33:43 -0500

    Email-only test

--- Assets/Tests/Player/EmailBlueprintTests.cs
+++ Assets/Tests/Player/EmailBlueprintTests.cs
@@ -8,21 +8,39 @@ namespace WitchOS.Tests
 {
     public class EmailBlueprintTests
     {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void EmailBlueprintTestsSimplePasses()
+        [SetUp]
+        public void SetUp ()
         {
-            // Use the Assert class to test conditions
+            Resources.Load<TimeState>("ScriptableObjects/Test Time State").Initialize();
         }
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator EmailBlueprintTestsWithEnumeratorPasses()
+        [Test]
+        public void BasicBlueprint_GeneratesSingleEmail ()
         {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
+            var blueprint = loadEmailBlueprint("Single Email");
+            Assert.That(blueprint.PossibleEmails.Items.Count, Is.EqualTo(1));
+
+            var email = blueprint.GenerateEmail();
+
+            Assert.That(email, Is.InstanceOf<Email>());
+            Assert.That(email, Is.Not.InstanceOf<Order>());
+
+            Assert.That(email.Body, Is.Not.Null);
+            Assert.That(email.Body, Is.Not.Empty);
+
+            Assert.That(email.SubjectLine, Is.Not.Null);
+            Assert.That(email.SubjectLine, Is.Not.Empty);
+
+            Assert.That(email.SenderAddress, Is.Not.Null);
+            Assert.That(email.SenderAddress, Is.Not.Empty);
+
+            var secondEmail = blueprint.GenerateEmail();
+            Assert.That(secondEmail, Is.EqualTo(email));
+        }
+
+        EmailBlueprint loadEmailBlueprint (string name)
+        {
+            return Resources.Load<EmailBlueprint>($"ScriptableObjects/Test EmailBlueprint - {name}");
         }
     }
 }
