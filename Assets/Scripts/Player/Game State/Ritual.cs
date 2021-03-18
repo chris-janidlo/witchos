845d61c3bd4eba56156eeea1508eae89c555e605
crass_sandwich <christopherjanidlo@gmail.com>
2021-03-18 14:51:12 -0400

    Basic skeleton code

---
+++ Assets/Scripts/Player/Game State/Ritual.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WitchOS
+{
+    [CreateAssetMenu(menuName = "WitchOS/Ritual", fileName = "newRitual.asset")]
+    public class Ritual : ScriptableObject
+    {
+        /// <summary>
+        /// The human-friendly name of the ritual this object represents, as opposed to <see cref="name"/>, which is the name of the object itself.
+        /// </summary>
+        public string Name;
+
+        [Tooltip("The emotion this ritual produces when rites that produce the proper affects are performed")]
+        public Emotion Product;
+
+        [Tooltip("The player must perform rites that produce these affects in the order they are listed here in order for this ritual to produce its Product")]
+        public List<Affect> RequiredAffects;
+    }
+}
