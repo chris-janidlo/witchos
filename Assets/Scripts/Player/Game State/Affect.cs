e63b2d7044c9b6815c2fc8b4cd3580f9c8910370
crass_sandwich <christopherjanidlo@gmail.com>
2021-03-18 18:02:13 -0400

    Affects don't really need an additional name field

--- Assets/Scripts/Player/Game State/Affect.cs
+++ Assets/Scripts/Player/Game State/Affect.cs
@@ -7,11 +7,6 @@ namespace WitchOS
     [CreateAssetMenu(menuName = "WitchOS/Affect", fileName = "newAffecct.asset")]
     public class Affect : ScriptableObject
     {
-        /// <summary>
-        /// The human-friendly name of the affect this object represents, as opposed to <see cref="name"/>, which is the name of the object itself.
-        /// </summary>
-        public string Name;
-
         [Tooltip("The rites that produce this affect when performed")]
         public List<Rite> ProducingRites;
     }
